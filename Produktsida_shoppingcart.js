c42384dbe737cd68963550c0be9b2ff9cb59637a
Kevin <kevinspring93@hotmail.com>
2019-11-25 15:14:03 +0100

    Kevin Save 6

---
+++ Produktsida_shoppingcart.js
@@ -0,0 +1,100 @@
+/* --------------------- SHOPPINGVAGNSKNAPP SOM LEDER EN TILL CHECKOUT ------------- */
+function shoppingKnapp (){
+    window.document.location="faktura.html";
+}
+const shoppingvagn = document.querySelector(".kundvagn");
+shoppingvagn.addEventListener("click", shoppingKnapp);
+
+
+
+/* ---------------------FUNKTION FÖR KNAPP 1 I PRODUKTLISTA------------------------ */
+function setItemValueBtn1 () {
+
+    const FirstProductPrice = document.querySelector(".cake__gridwrapper__item-price-1").innerHTML;
+    const FirstProductHeader = document.querySelector(".cake__gridwrapper__item-h3-1").innerHTML;
+    const FirstProductImg = document.querySelector(".cake__gridwrapper__item-img-1").src;
+    localStorage.setItem("FirstProductPrice", FirstProductPrice);
+    localStorage.setItem("FirstProductHeader", FirstProductHeader);
+    localStorage.setItem("FirstProductImg", FirstProductImg);
+    /* window.document.location="faktura.html"; */
+
+}
+const FirstProductBtn = document.querySelector(".cake__gridwrapper__item-btn-1");
+FirstProductBtn.addEventListener("click", setItemValueBtn1);
+
+
+/* ---------------------FUNKTION FÖR KNAPP 2 I PRODUKTLISTA------------------------ */
+function setItemValueBtn2 () {
+
+    const SecondProductPrice = document.querySelector(".cake__gridwrapper__item-price-2").innerHTML;
+    const SecondProductHeader = document.querySelector(".cake__gridwrapper__item-h3-2").innerHTML;
+    const SecondProductImg = document.querySelector(".cake__gridwrapper__item-img-2").src;
+    localStorage.setItem("SecondProductPrice", SecondProductPrice);
+    localStorage.setItem("SecondProductHeader", SecondProductHeader);
+    localStorage.setItem("SecondProductImg", SecondProductImg);
+    
+
+}
+const SecondProductBtn = document.querySelector(".cake__gridwrapper__item-btn-2");
+SecondProductBtn.addEventListener("click", setItemValueBtn2);
+
+/* ---------------------FUNKTION FÖR KNAPP 3 I PRODUKTLISTA------------------------ */
+function setItemValueBtn3 () {
+
+    const ThirdProductPrice = document.querySelector(".cake__gridwrapper__item-price-3").innerHTML;
+    const ThirdProductHeader = document.querySelector(".cake__gridwrapper__item-h3-3").innerHTML;
+    const ThirdProductImg = document.querySelector(".cake__gridwrapper__item-img-3").src;
+    localStorage.setItem("ThirdProductPrice", ThirdProductPrice);
+    localStorage.setItem("ThirdProductHeader", ThirdProductHeader);
+    localStorage.setItem("ThirdProductImg", ThirdProductImg);
+    
+
+}
+const ThirdProductBtn = document.querySelector(".cake__gridwrapper__item-btn-3");
+ThirdProductBtn.addEventListener("click", setItemValueBtn3);
+
+/* ---------------------FUNKTION FÖR KNAPP 4 I PRODUKTLISTA------------------------ */
+function setItemValueBtn4 () {
+
+    const FourthProductPrice = document.querySelector(".cake__gridwrapper__item-price-4").innerHTML;
+    const FourthProductHeader = document.querySelector(".cake__gridwrapper__item-h3-4").innerHTML;
+    const FourthProductImg = document.querySelector(".cake__gridwrapper__item-img-4").src;
+    localStorage.setItem("FourthProductPrice", FourthProductPrice);
+    localStorage.setItem("FourthProductHeader", FourthProductHeader);
+    localStorage.setItem("FourthProductImg", FourthProductImg);
+    
+
+}
+const FourthProductBtn = document.querySelector(".cake__gridwrapper__item-btn-4");
+FourthProductBtn.addEventListener("click", setItemValueBtn4);
+
+/* ---------------------FUNKTION FÖR KNAPP 5 I PRODUKTLISTA------------------------ */
+function setItemValueBtn5 () {
+
+    const FifthProductPrice = document.querySelector(".cake__gridwrapper__item-price-5").innerHTML;
+    const FifthProductHeader = document.querySelector(".cake__gridwrapper__item-h3-5").innerHTML;
+    const FifthProductImg = document.querySelector(".cake__gridwrapper__item-img-5").src;
+    localStorage.setItem("FifthProductPrice", FifthProductPrice);
+    localStorage.setItem("FifthProductHeader", FifthProductHeader);
+    localStorage.setItem("FifthProductImg", FifthProductImg);
+    
+
+}
+const FifthProductBtn = document.querySelector(".cake__gridwrapper__item-btn-5");
+FifthProductBtn.addEventListener("click", setItemValueBtn5);
+
+/* ---------------------FUNKTION FÖR KNAPP 6 I PRODUKTLISTA------------------------ */
+function setItemValueBtn6 () {
+
+    const SixthProductPrice = document.querySelector(".cake__gridwrapper__item-price-6").innerHTML;
+    const SixthProductHeader = document.querySelector(".cake__gridwrapper__item-h3-6").innerHTML;
+    const SixthProductImg = document.querySelector(".cake__gridwrapper__item-img-6").src;
+    localStorage.setItem("SixthProductPrice", SixthProductPrice);
+    localStorage.setItem("SixthProductHeader", SixthProductHeader);
+    localStorage.setItem("SixthProductImg", SixthProductImg);
+    
+
+}
+const SixthProductBtn = document.querySelector(".cake__gridwrapper__item-btn-6");
+SixthProductBtn.addEventListener("click", setItemValueBtn6);
+
---
+++ faktura.js
@@ -0,0 +1,55 @@
+function ShowData(){
+    const fakturaPris1 = document.querySelector(".fakturapris1");
+    const fakturaPris2 = document.querySelector(".fakturapris2");
+    const fakturaPris3 = document.querySelector(".fakturapris3");
+    const fakturaPris4 = document.querySelector(".fakturapris4");
+    const fakturaPris5 = document.querySelector(".fakturapris5");
+    const fakturaPris6 = document.querySelector(".fakturapris6");
+
+    const productheader1 = document.querySelector(".productheader1");
+    const productheader2 = document.querySelector(".productheader2");
+    const productheader3 = document.querySelector(".productheader3");
+    const productheader4 = document.querySelector(".productheader4");
+    const productheader5 = document.querySelector(".productheader5");
+    const productheader6 = document.querySelector(".productheader6");
+
+    const productImage1 = document.querySelector(".productImage1");
+    const productImage2 = document.querySelector(".productImage2");
+    const productImage3 = document.querySelector(".productImage3");
+    const productImage4 = document.querySelector(".productImage4");
+    const productImage5 = document.querySelector(".productImage5");
+    const productImage6 = document.querySelector(".productImage6");
+   
+    fakturaPris1.innerHTML= localStorage.getItem("FirstProductPrice");
+    productheader1.innerHTML = localStorage.getItem("FirstProductHeader");
+    productImage1.src = localStorage.getItem("FirstProductImg");
+
+    fakturaPris2.innerHTML = localStorage.getItem("SecondProductPrice");
+    productheader2.innerHTML = localStorage.getItem("SecondProductHeader");
+    productImage2.src = localStorage.getItem("SecondProductImg");
+
+    fakturaPris3.innerHTML = localStorage.getItem("ThirdProductPrice");
+    productheader3.innerHTML = localStorage.getItem("ThirdProductHeader");
+    productImage3.src = localStorage.getItem("ThirdProductImg");
+
+    fakturaPris4.innerHTML = localStorage.getItem("FourthProductPrice");
+    productheader4.innerHTML = localStorage.getItem("FourthProductHeader");
+    productImage4.src = localStorage.getItem("FourthProductImg");
+
+    fakturaPris5.innerHTML = localStorage.getItem("FifthProductPrice");
+    productheader5.innerHTML = localStorage.getItem("FifthProductHeader");
+    productImage5.src = localStorage.getItem("FifthProductImg");
+
+    fakturaPris6.innerHTML = localStorage.getItem("SixthProductPrice");
+    productheader6.innerHTML = localStorage.getItem("SixthProductHeader");
+    productImage6.src = localStorage.getItem("SixthProductImg");
+
+
+    
+  }
+
+  //DOMContentLoaded event väntar tills documentet laddas upp helt sedan anropar 
+  //functionen inuti den
+  document.addEventListener("DOMContentLoaded", function (){
+    ShowData();
+ })
